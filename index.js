b4c4d1fbe1abe3c3dbd09b42119714c9c13c2986
unknown <202215849@smcsdom.local>
2025-03-10 14:45:45 +0200

    added the read property for the CRUD

--- index.js
+++ index.js
@@ -9,18 +9,23 @@ class Note{
 class App{
     constructor(notes){
         this.notes = [];
-        
-        this.$activeForm = document.querySelector(".active-form");
+
         this.$inactiveForm = document.querySelector(".inactive-form");
+        this.$activeForm = document.querySelector(".active-form");
         this.$noteTitle = document.querySelector("#note-title");
         this.$noteText = document.querySelector("#note-text");
+        this.$notes = document.querySelector(".notes");
 
         this.addEventListeners();
     }
 
-    addNote(id, {title, text}){
-        let newNote = new Note(id, title, text);
-        this.notes = [...this.notes, newNote];
+    addNote({title, text}){
+        if(this.$noteTitle.value !== "" && this.$noteText.value !== ""){
+            let newNote = new Note(cuid(), title, text);
+            this.notes = [...this.notes, newNote];
+            this.displayNote();
+        }
+
     }
 
     editNote(id, {title, text}){
@@ -39,41 +44,77 @@ class App{
     }
 
     displayNote(){
-        this.notes.map((item) =>{
-            console.log(`
-                ID: ${item.id}
-                Title: ${item.title}
-                Text: ${item.text}`);
-        });
+        this.$notes.innerHTML = this.notes.map((item) =>{
+        return   `
+                    <div class="note" id="${item.id}">
+                        <span class="material-symbols-outlined check-circle">check_circle</span>
+                        <div class="title">${item.title}</div>
+                        <div class="text">${item.text}</div>
+                        <div class="note-footer">
+                            <div class="tooltip">
+                                <span class="material-symbols-outlined hover small-icons">add_alert</span>
+                                <span class="tooltip-text">Remind me</span>
+                            </div>
+                            <div class="tooltip">
+                                <span class="material-symbols-outlined hover small-icons">person_add</span>
+                                <span class="tooltip-text">Collaborator</span>
+                            </div>
+                            <div class="tooltip">
+                                <span class="material-symbols-outlined hover small-icons">palette</span>
+                                <span class="tooltip-text">Change color</span>
+                            </div>
+                            <div class="tooltip">
+                                <span class="material-symbols-outlined hover small-icons">image</span>
+                                <span class="tooltip-text">Image</span>
+                            </div>
+                            <div class="tooltip">
+                                <span class="material-symbols-outlined hover small-icons">archive</span>
+                                <span class="tooltip-text">Archive</span>
+                            </div>
+                            <div class="tooltip">
+                                <span class="material-symbols-outlined hover small-icons">more_vert</span>
+                                <span class="tooltip-text">More</span>
+                            </div>
+                        </div>
+                    </div>
+                 `
+        }).join("");
     }
-
+ 
     addEventListeners(){
         document.body.addEventListener("click", (event) =>{
             this.handleFormClick(event);
         });
-    }
+    };
 
-    handleFormClick(event){
+    handleFormClick(){
         let isInactiveFormClickedOn = this.$inactiveForm.contains(event.target);
         let isActiveFormClickedOn = this.$activeForm.contains(event.target);
 
+        let title = this.$noteTitle.value;
+        let text = this.$noteText.value;
         if(isInactiveFormClickedOn){
             this.openActiveForm();
         }
-        else if(!isInactiveFormClickedOn  && !isActiveFormClickedOn){
+
+        else if(!isInactiveFormClickedOn && !isActiveFormClickedOn){
+            this.addNote({title, text});
             this.closeActiveForm();
         }
-    }
+    };
 
     openActiveForm(){
-        this.$inactiveForm.style.display = "none";
         this.$activeForm.style.display = "block";
+        this.$inactiveForm.style.display = "none";
         this.$noteText.focus();
     }
 
     closeActiveForm(){
         this.$inactiveForm.style.display = "block";
         this.$activeForm.style.display = "none";
+
+        this.$noteTitle.value = "";
+        this.$noteText.value = "";
     }
 }
 
