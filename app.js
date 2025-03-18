e639c8521ced51122a0b8ee7f710afee52f7357c
unknown <202215849@smcsdom.local>
2025-03-18 15:26:08 +0200

    firestore database

--- app.js
+++ app.js
@@ -273,7 +273,7 @@ class App{
         this.ui.start('#firebaseui-auth-container', {
             signInOptions: [
               firebase.auth.EmailAuthProvider.PROVIDER_ID,
-            //   firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+              firebase.auth.GoogleAuthProvider.PROVIDER_ID,
             ],
             // Other config options...
           });
